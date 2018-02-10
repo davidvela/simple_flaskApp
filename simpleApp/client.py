99528b8ecafee8cbad4ae850b0bf5e452e9f8591
david <david.vela.tirado@gmail.com>
2018-02-11 00:59:36 +0100

    test API - flask

---
+++ simpleApp/client.py
@@ -0,0 +1,28 @@
+from flask import *
+import pandas as pd
+from requests import put, get
+
+app = Flask(__name__)
+app.config['SERVER_NAME'] = '127.0.0.1' + ':' + '5002'
+# flask_app.config['SERVER_NAME'] = server_name + ':' + server_port
+# flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION # 'list'
+# flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE # True
+# flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER # False
+# flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP # False
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    # user = {'username': 'David_A'}
+    # return render_template('index.html', title='Home', user=user)
+    # print(get('http://localhost:5000/hello').json())
+    # return "Hello, World!"
+    ret = get('http://localhost:5000/hello').json()
+    print(ret)
+    return "hello__" + ret["hello"] #"Hello, World!"
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  
--- simpleREST/_restplus_client.py
+++ simpleREST/_restplus_client.py
@@ -1,10 +1,22 @@
 from requests import put, get
 
-ret = put('http://localhost:5000/todo1', data={'data': 'Remember the milk2'}).json()
-print(ret) # {u'todo1': u'Remember the milk'}
-ret = get('http://localhost:5000/todo1').json()
-print(ret) # {u'todo1': u'Remember the milk'}
-ret = put('http://localhost:5000/todo2', data={'data': 'Change my brakepads'}).json()
-print(ret) # {u'todo2': u'Change my brakepads'}
-ret = get('http://localhost:5000/todo2').json()
-print(ret) # {u'todo2': u'Change my brakepads'}
+def main():
+    # print("hi");  return
+    
+    ret = get('http://localhost:5000/hello').json()
+    print(ret) # hello world 
+    return
+
+    ret = put('http://localhost:5000/todo1', data={'data': 'Remember the milk2'}).json()
+    print(ret) # {u'todo1': u'Remember the milk'}
+
+    ret = get('http://localhost:5000/todo1').json()
+    print(ret) # {u'todo1': u'Remember the milk'}
+    ret = put('http://localhost:5000/todo2', data={'data': 'Change my brakepads'}).json()
+    print(ret) # {u'todo2': u'Change my brakepads'}
+    ret = get('http://localhost:5000/todo2').json()
+    print(ret) # {u'todo2': u'Change my brakepads'}
+
+    
+if __name__ == "__main__":
+    main()
