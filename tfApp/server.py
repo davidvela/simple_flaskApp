95a11bedbb4047fe226a9f74fc9872925877b119
david <david.vela.tirado@gmail.com>
2018-02-16 18:11:51 +0100

    HTML - request: REST

---
+++ tfApp/server.py
@@ -0,0 +1,16 @@
+from flask import *
+from flask_restplus import Api, Resource, reqparse
+# from flask_cors import CORS  
+# #https://github.com/corydolphin/flask-cors/issues/201
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/hello') 
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    # app.run(debug=True) # Debug mode should never be used in a production environment!
+    app.run(host='0.0.0.0')
