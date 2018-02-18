6ffd64257cecf087f3a0bbc5a876e7297dc1ead1
david <david.vela.tirado@gmail.com>
2018-02-18 02:55:15 +0100

    cors error fix!

--- tfApp/server.py
+++ tfApp/server.py
@@ -1,10 +1,11 @@
 from flask import *
 from flask_restplus import Api, Resource, reqparse
-# from flask_cors import CORS  
+from flask_cors import CORS  
 # #https://github.com/corydolphin/flask-cors/issues/201
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 @api.route('/hello') 
 class HelloWorld(Resource):
